f97d5fb0fa158895cdf870beaa87762a7e98acb8
Molydsxz <119066192+Molydsxz@users.noreply.github.com>
2022-12-24 10:47:54 +0300

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -52,7 +52,7 @@ local ThemeManager = {} do
 	end
 
 	function ThemeManager:LoadDefault()		
-		local theme = 'Bitch Bot'
+		local theme = 'Tokyo Night'
 		local content = isfile(self.Folder .. '/themes/default.txt') and readfile(self.Folder .. '/themes/default.txt')
 
 		local isDefault = true
